fe6ce6eeb0158965598d363738d0d0b309e661ae
maddrake220 <jwchoi4118@gmail.com>
2021-11-25 15:27:56 +0900

    docs: already done

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,4 +3,5 @@ for(let i = 1; i<= 100; i++)
 	i % 3 == 0 ? i % 5 == 0 ? console.log("fizzbuzz") : console.log("fizz") : i % 5 == 0 ? console.log("buzz") :  console.log(i)
 }
 // all done
+// fizzbuzz already done@
 
